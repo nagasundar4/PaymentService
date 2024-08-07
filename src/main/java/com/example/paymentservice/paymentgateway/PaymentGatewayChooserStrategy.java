312fcdc3811bdaceed8911b060ee6e7111572b33
Nagarajan Sundaram <nagasundar@gmail.com>
2024-08-07 11:48:44 -0400

    Adding stripe and webhook.

--- src/main/java/com/example/paymentservice/paymentgateway/PaymentGatewayChooserStrategy.java
+++ src/main/java/com/example/paymentservice/paymentgateway/PaymentGatewayChooserStrategy.java
@@ -2,13 +2,24 @@ package com.example.paymentservice.paymentgateway;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.Random;
 
 @Component
 public class PaymentGatewayChooserStrategy {
+
     @Autowired
     private RazorpayPaymentGateway razorpayPaymentGateway;
 
+    @Autowired
+    private StripePaymentGateway stripePaymentGateway;
+
     public PaymentGateway getOptimalPaymentGateway() {
-        return razorpayPaymentGateway;
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        if (randomNumber % 2 == 0) {
+            return stripePaymentGateway;
+        } else {
+            return razorpayPaymentGateway;
+        }
     }
 }
